29e1781fb294d706e31b3068b9f13d5054aec20b
jujuGthb <julianaromialison3@gmail.com>
2026-02-20 15:36:07 +0300

    changed

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -1,86 +1,298 @@
+
 from pydantic import Field, validator
 from typing import List, Optional, Union, Literal
-from sdks.novavision.src.base.model import (
-    Package, Image, Inputs, Configs, Outputs, 
-    Response, Request, Output, Input, Config
-)
+from sdks.novavision.src.base.model import Package, Image, Inputs, Configs, Outputs, Response, Request, Output, Input, Config
 
-# --- INPUTS ---
+
+# Inputs 
 class InputImage(Input):
     name: Literal["inputImage"] = "inputImage"
     value: Union[List[Image], Image]
     type: str = "object"
 
     @validator("type", pre=True, always=True)
-    def set_type_based_on_value(cls, v, values):
-        val = values.get('value')
-        return "list" if isinstance(val, list) else "object"
+    def set_type_based_on_value(cls, type_value, values):
+        actual_value = values.get('value')
+        if isinstance(actual_value, Image):
+            return "object"
+        elif isinstance(actual_value, list):
+            return type_value
 
     class Config:
-        title = "Primary Image"
+        title = "Image"
+class InputImage2(Input):
+    name: Literal["inputImage2"] = "inputImage2"
+    value: Union[List[Image], Image]
+    type: str = "object"
+    Optional: bool = True
 
-# --- CONFIGS (Bottom-Up) ---
+    @validator("type", pre=True, always=True)
+    def set_type_based_on_value(cls, type_value, values):
+        actual_value = values.get('value')
+        if isinstance(actual_value, Image):
+            return "object"
+        elif isinstance(actual_value, list):
+            return type_value
+
+    class Config:
+        title = "Second Image"
+    
+
+# Outputs
+
+class OutputImage(Output):
+    name: Literal["outputImage"] = "outputImage"
+    value: Union[List[Image],Image]
+    type: str = "object"
+
+    @validator("type", pre=True, always=True)
+    def set_type_based_on_value(cls, type_value, values):
+        actual_value = values.get('value')
+        if isinstance(actual_value, Image):
+            return "object"
+        elif isinstance(actual_value, list):
+            return type_value
+
+    class Config:
+        title = "Image"
+
+class OutputImage2(Output):
+    name: Literal["outputImage2"] = "outputImage2"
+    value: Union[List[Image], Image]
+    type: str = "object"
+    @validator("type", pre=True, always=True)
+    def set_type_based_on_value(cls, type_value, values):
+        actual_value = values.get('value')
+        if isinstance(actual_value, Image): return "object"
+        elif isinstance(actual_value, list): return "list"
+        return type_value
+    class Config: title = "Secondary Output"
+    
+
+
+#  Parameters
+
+# **********for the first executor
+class SimpleText(Config):
+    name: Literal["Text"]= "Text"
+    value:str
+    type: Literal["string"] = "string"
+    field: Literal["textInput"] = "textInput"
+    
+    class Config:
+        title = "Text"
+
 class SimpleNumber(Config):
-    name: Literal["SimpleNumber"] = "SimpleNumber"
-    value: int = Field(default=0)
+    name: Literal["Number"]= "Number"
+    value: int
     type: Literal["number"] = "number"
     field: Literal["textInput"] = "textInput"
+    
     class Config:
-        title = "Number Value"
+        title = "Number"
 
-class OptionGrayscale(Config):
-    name: Literal["optionGrayscale"] = "optionGrayscale" # Optionlar camelCase
-    value: SimpleNumber
+
+class EnableFlag(Config):
+    name: Literal["Enable"] = "Enable"
+    value: Literal[True] = True
+    type: Literal["bool"] = "bool"
+    field: Literal["option"] = "option"
+
+    class Config:
+        title = "Enable"
+    
+class ModeSelect(Config):
+    name: Literal["Mode"] = "Mode"
+    value: Literal["Mode1", "Mode2", "Mode3"] = "Mode1"
+    type: Literal["string"] = "string"
+    field: Literal["selectBox"] = "selectBox"
+    
+    
+class Grayscale(Config):
+    name: Literal["Grayscale"] = "Grayscale"
+    value: Union[SimpleText, SimpleNumber]
+    type: Literal["object"] = "object"
+    field: Literal["option"] = "option"
+
+    class Config:
+        title = "Convert to Grayscale"
+        json_schema_extra = {
+            "target": "value"
+        }
+class FlipHorizontal(Config):
+    name: Literal["Flip Horizontal"] = "Flip Horizontal"
+    value: Union[EnableFlag, ModeSelect]
+    type: Literal["object"] = "object"
+    field: Literal["option"] = "option"
+
+    class Config:
+        title = "Flip Horizontally"
+        json_schema_extra = {
+            "target": "value"
+        }
+
+class Operation(Config):
+    name: Literal["Operation"] = "Operation"
+    value: Union[Grayscale, FlipHorizontal]
+    type: Literal["object"] = "object"
+    field: Literal["dependentDropdownlist"] = "dependentDropdownlist"
+    
+    class Config:
+        title = "Operation"
+        json_schema_extra = {
+            "target": "value"
+        }
+        
+# ***********For second executor
+class ScaleValue(Config):
+    name: Literal["Scale"] = "Scale"
+    value: float = 0.5
+    type: Literal["number"] = "number"
+    field: Literal["slider"] = "slider"
+    
+    class Config:
+        title = "Scale Factor"
+
+class ResizeMethod(Config):
+    name: Literal["Method"] = "Method"
+    value: Literal["area", "linear", "cubic"] = "area"
+    type: Literal["string"] = "string"
+    field: Literal["selectBox"] = "selectBox"
+    
+    class Config:
+        title = "Interpolation"
+
+class RotateEnable(Config):
+    name: Literal["Enable"] = "Enable"
+    value: bool = True
+    type: Literal["bool"] = "bool"
+    field: Literal["option"] = "option"
+    class Config:
+        title = "Enable Rotation"
+
+class RotateAngle(Config):
+    name: Literal["Angle"] = "Angle"
+    value: int = 90
+    type: Literal["number"] = "number"
+    field: Literal["slider"] = "slider"
+    class Config:
+        title = "Angle Degrees"
+
+class ResizeMode(Config):
+    name: Literal["Resize"] = "Resize"
+    value: Union[ScaleValue, ResizeMethod]
     type: Literal["object"] = "object"
     field: Literal["option"] = "option"
     class Config:
-        title = "Grayscale Mode"
+        title = "Resize Image"
         json_schema_extra = {"target": "value"}
 
-class ConfigOperation(Config):
-    name: Literal["ConfigOperation"] = "ConfigOperation" # Ana config PascalCase
-    value: Union[OptionGrayscale] 
+class RotateMode(Config):
+    name: Literal["Rotate"] = "Rotate"
+    value: Union[RotateEnable, RotateAngle]
+    type: Literal["object"] = "object"
+    field: Literal["option"] = "option"
+    class Config:
+        title = "Rotate Image"
+        json_schema_extra = {
+            "target": "value"
+            }
+
+class ProcessingMode(Config):
+    name: Literal["ProcessingMode"] = "ProcessingMode"
+    value: Union[ResizeMode, RotateMode]
     type: Literal["object"] = "object"
     field: Literal["dependentDropdownlist"] = "dependentDropdownlist"
     class Config:
-        title = "Operation Select"
-        json_schema_extra = {"shortDescription": "Select processing task", "target": "value"}
+        title = "Processing Mode"
+        json_schema_extra = {
+            "target": "value"
+            }
 
-# --- EXECUTOR REQUEST/RESPONSE ---
+    
 class FirstExecutorInputs(Inputs):
     inputImage: InputImage
-
+    
+class SecondExecutorInputs(Inputs):
+    inputImage1: InputImage 
+    inputImage2: InputImage2 
+    
 class FirstExecutorConfigs(Configs):
-    operation: ConfigOperation
+    operation: Operation
+    
+class SecondExecutorConfigs(Configs):
+    processingMode: ProcessingMode
+
 
 class FirstExecutorRequest(Request):
     inputs: Optional[FirstExecutorInputs]
     configs: FirstExecutorConfigs
+
     class Config:
-        json_schema_extra = {"target": "configs"}
+        json_schema_extra = {
+            "target": "configs"
+        }
+        
+class SecondExecutorRequest(Request):
+    inputs: Optional[SecondExecutorInputs]
+    configs: SecondExecutorConfigs
+    class Config: json_schema_extra = {
+        "target": "configs"
+        }
+class FirstExecutorOutputs(Outputs):
+    outputImage: OutputImage
 
+class SecondExecutorOutputs(Outputs):
+    outputImage1: OutputImage  
+    outputImage2: OutputImage2 
+    
 class FirstExecutorResponse(Response):
-    outputs: Outputs # Basitleştirilmiş veya özel output sınıfı
+    outputs: FirstExecutorOutputs
 
-# --- TOP LEVEL ---
+class SecondExecutorResponse(Response):
+    outputs: SecondExecutorOutputs
+
 class FirstExecutor(Config):
     name: Literal["FirstExecutor"] = "FirstExecutor"
     value: Union[FirstExecutorRequest, FirstExecutorResponse]
     type: Literal["object"] = "object"
     field: Literal["option"] = "option"
+    
     class Config:
-        title = "Image Processor"
-        json_schema_extra = {"target": {"value": 0}} # Her zaman Request'i hedefle
+        title = "FirstExecutor"
+       
+
+
+class SecondExecutor(Config):
+    name: Literal["SecondExecutor"] = "SecondExecutor"
+    value: Union[SecondExecutorRequest, SecondExecutorResponse]
+    type: Literal["object"] = "object"
+    field: Literal["option"] = "option"
+    class Config:
+        title = "SecondExecutor"
+        
+
 
 class ConfigExecutor(Config):
     name: Literal["ConfigExecutor"] = "ConfigExecutor"
-    value: Union[FirstExecutor] # Diğer executorları buraya ekle
+    value: Union[FirstExecutor,SecondExecutor]
     type: Literal["executor"] = "executor"
     field: Literal["dependentDropdownlist"] = "dependentDropdownlist"
+
     class Config:
-        title = "Task selection"
+        title = "Task"
+        json_schema_extra = {   
+             "shortDescription": "gray scale flip horizontal"
+        }
+        
+        
+
+
+class PackageConfigs(Configs):
+    executor: ConfigExecutor
+
 
 class PackageModel(Package):
-    configs: Configs
+    configs: PackageConfigs
     type: Literal["component"] = "component"
-    name: Literal["ImageProcessingPackage"] = "ImageProcessingPackage"
+    name: Literal["GrayScaleFlipHorizontal"] = "GrayScaleFlipHorizontal"
